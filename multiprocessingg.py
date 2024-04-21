ff4f002bff73d50f89952c63784b47d1a836a365
whitcr <whitcr0k@gmail.com>
2024-04-21 21:44:18 +0200

    added

--- multiprocessingg.py
+++ multiprocessingg.py
@@ -6,43 +6,54 @@ KEYWORDS = ["python", "programming", "threads", "processes"]
 
 
 def find_keywords_in_file(file_path, keywords, results):
-    with open(file_path, "r", encoding="utf-8") as file:
-        content = file.read().lower()
-        for keyword in keywords:
-            if re.search(keyword, content):
-                results[keyword].append(file_path)
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            content = file.read().lower()
+            for keyword in keywords:
+                if re.search(keyword, content):
+                    results[keyword].append(file_path)
+    except FileNotFoundError as e:
+        print(f"File not found: {file_path}. Error: {e}")
+    except Exception as e:
+        print(f"An error occurred while processing file: {file_path}. Error: {e}")
 
 
 def multiprocessing_file_processing(files, keywords):
+    try:
+        manager = Manager()
+        results = {keyword: manager.list() for keyword in keywords}
+        processes = []
 
-    manager = Manager()
-    results = {keyword: manager.list() for keyword in keywords}
-    processes = []
+        for file in files:
+            process = Process(
+                target=find_keywords_in_file, args=(file, keywords, results)
+            )
+            processes.append(process)
+            process.start()
 
-    for file in files:
-        process = Process(target=find_keywords_in_file, args=(file, keywords, results))
-        processes.append(process)
-        process.start()
+        for process in processes:
+            process.join()
 
-    for process in processes:
-        process.join()
+        results_dict = {
+            keyword: list(file_paths) for keyword, file_paths in results.items()
+        }
 
-    results_dict = {
-        keyword: list(file_paths) for keyword, file_paths in results.items()
-    }
-
-    return results_dict
+        return results_dict
+    except Exception as e:
+        print(f"An error occurred while processing. Error: {e}")
 
 
 if __name__ == "__main__":
+    try:
+        files = [f"file{i}.txt" for i in range(1, 4)]
 
-    files = [f"file{i}.txt" for i in range(1, 4)]
+        print("Багатопроцесорний підхід:")
+        import time
 
-    print("Багатопроцесорний підхід:")
-    import time
-
-    start_time = time.time()
-    multiprocessing_results = multiprocessing_file_processing(files, KEYWORDS)
-    end_time = time.time()
-    print("Час виконання:", end_time - start_time, "секунд")
-    print(multiprocessing_results)
+        start_time = time.time()
+        multiprocessing_results = multiprocessing_file_processing(files, KEYWORDS)
+        end_time = time.time()
+        print("Час виконання:", end_time - start_time, "секунд")
+        print(multiprocessing_results)
+    except Exception as e:
+        print(f"Error: {e}")
--- multithreaded.py
+++ multithreaded.py
@@ -1,44 +1,54 @@
 import re
+from collections import defaultdict
 from threading import Thread
-
 
 KEYWORDS = ["python", "programming", "threads", "processes"]
 
 
 def find_keywords_in_file(file_path, keywords, results):
-
-    with open(file_path, "r", encoding="utf-8") as file:
-        content = file.read().lower()
-        for keyword in keywords:
-            if re.search(keyword, content):
-                results[keyword].append(file_path)
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            content = file.read().lower()
+            for keyword in keywords:
+                if re.search(keyword, content):
+                    results[keyword].append(file_path)
+    except FileNotFoundError as e:
+        print(f"File not found: {file_path}. Error: {e}")
+    except Exception as e:
+        print(f"An error occurred while processing file: {file_path}. Error: {e}")
 
 
 def multithreaded_file_processing(files, keywords):
+    try:
+        results = defaultdict(list)
+        threads = []
 
-    results = []
-    threads = []
+        for file in files:
+            thread = Thread(
+                target=find_keywords_in_file, args=(file, keywords, results)
+            )
+            threads.append(thread)
+            thread.start()
 
-    for file in files:
-        thread = Thread(target=find_keywords_in_file, args=(file, keywords, results))
-        threads.append(thread)
-        thread.start()
+        for thread in threads:
+            thread.join()
 
-    for thread in threads:
-        thread.join()
-
-    return results
+        return dict(results)
+    except Exception as e:
+        print(f"An error occurred while processing. Error: {e}")
 
 
 if __name__ == "__main__":
+    try:
+        files = [f"file{i}.txt" for i in range(1, 4)]
 
-    files = [f"file{i}.txt" for i in range(1, 4)]
+        print("Багатопотоковий підхід:")
+        import time
 
-    print("Багатопотоковий підхід:")
-    import time
-
-    start_time = time.time()
-    multithreaded_results = multithreaded_file_processing(files, KEYWORDS)
-    end_time = time.time()
-    print("Час виконання:", end_time - start_time, "секунд")
-    print(multithreaded_results)
+        start_time = time.time()
+        multithreaded_results = multithreaded_file_processing(files, KEYWORDS)
+        end_time = time.time()
+        print("Час виконання:", end_time - start_time, "секунд")
+        print(multithreaded_results)
+    except Exception as e:
+        print(f"Error: {e}")
